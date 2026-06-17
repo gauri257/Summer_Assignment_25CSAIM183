3d00369bebff2cc08c1c2cb7c304dc6c6644ef96
gauri257 <gauri.kumari2025@glbajajgroup.org>
2026-06-17 17:36:02 +0530

    summer assignment Day-9

--- program1.c
+++ program1.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int fact(int n)
-{
-    if(n == 0)
-        return 1;
-    return n * fact(n - 1);
-}
+int main() {
+    int i, j;
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    printf("%d", fact(n));
+    for(i = 5; i >= 1; i--) {
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
