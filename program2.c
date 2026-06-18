7ea8f308f2ca67480522b044dc15962b4adc81cd
gauri257 <gauri.kumari2025@glbajajgroup.org>
2026-06-18 17:53:12 +0530

    summer assignment Day-10

--- program2.c
+++ program2.c
@@ -4,8 +4,8 @@ int main() {
     int i, j;
 
     for(i = 5; i >= 1; i--) {
-        for(j = 1; j <= i; j++) {
-            printf("%d", j);
+        for(j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
         }
         printf("\n");
     }
--- program3.c
+++ program3.c
@@ -5,8 +5,12 @@ int main() {
 
     for(i = 1; i <= 5; i++) {
         for(j = 1; j <= i; j++) {
-            printf("%c", 'A' + i - 1);
+            printf("%d", j);
         }
+        for(j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
         printf("\n");
     }
 
